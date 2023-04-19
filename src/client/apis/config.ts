f84d11b0b2eaf9c6dd212e5b53e020aa079774e3
Niv Eini <niv@hiredscore.com>
2023-04-19 18:38:05 +0300

    + Organize

--- src/client/apis/config.ts
+++ src/client/apis/config.ts
@@ -1,4 +1,5 @@
-const BASE_API = "http://localhost:5000"
+const BASE_API = process.env.URL || "http://localhost:5000"
+
 export const APIS = {
   ALL_MOVIES: `${BASE_API}/movies`,
   SINGLE_MOVIE: (movieId: string) => `${BASE_API}/movies/${movieId}`,
